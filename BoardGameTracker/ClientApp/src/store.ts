a5b704f88a293cbe972e2c7aa0a1a733240a4038
Tom Smith <tom.smith2711@gmail.com>
2018-12-31 15:33:23 +0000

    also fix name not showing top right

--- BoardGameTracker/ClientApp/src/store.ts
+++ BoardGameTracker/ClientApp/src/store.ts
@@ -5,24 +5,24 @@ import { User } from './models/User';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    user: {},
-  },
-  mutations: {
-    setUser(state, user: User) {
-      state.user = user;
-      localStorage.setItem('user', JSON.stringify(user));
-      httpClient.defaults.headers.Authorization = `Bearer ${(state.user as any).token}`;
-    },
-    logout(state) {
-      state.user = {};
-      localStorage.removeItem('user');
-    },
-  },
-  actions: {},
-  getters: {
-    loggedIn(state: any) {
-      return state.user.id == true;
-    },
-  },
+	state: {
+		user: {}
+	},
+	mutations: {
+		setUser(state, user: User) {
+			state.user = user;
+			localStorage.setItem('user', JSON.stringify(user));
+			httpClient.defaults.headers.Authorization = `Bearer ${(state.user as any).token}`;
+		},
+		logout(state) {
+			state.user = {};
+			localStorage.removeItem('user');
+		}
+	},
+	actions: {},
+	getters: {
+		loggedIn(state: any) {
+			return state.user.id !== undefined;
+		}
+	}
 });
