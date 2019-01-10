4b17f065f780187f21fae4f13102505a0c659b04
Tom Smith <tom.smith2711@gmail.com>
2019-01-10 01:29:11 +0000

    update game session list ui

--- BoardGameTracker/ClientApp/src/views/GameSessionCreate.vue
+++ BoardGameTracker/ClientApp/src/views/GameSessionCreate.vue
@@ -8,7 +8,7 @@
       <v-flex>
         <v-dialog ref="dialog" v-model="modal" :return-value.sync="date" persistent lazy full-width width="290px">
           <v-text-field slot="activator" v-model="date" label="Date Played" readonly></v-text-field>
-          <v-date-picker v-model=" date" scrollable>
+          <v-date-picker v-model="date" scrollable>
             <v-spacer></v-spacer>
             <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
             <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
--- BoardGameTracker/ClientApp/src/views/GameSessionList.vue
+++ BoardGameTracker/ClientApp/src/views/GameSessionList.vue
@@ -5,46 +5,48 @@
       <v-btn color="primary">New</v-btn>
     </router-link>
     <v-progress-circular v-if="isLoading" :size="50" color="primary" indeterminate></v-progress-circular>
-    <v-timeline dense>
-      <v-timeline-item :key="gameSessionGroup.id" v-for="gameSessionGroup in gameSessionGroups" color="primary lighten-1" fill-dot left small>
-        <v-card>
-          <v-card-title class="primary lighten-1">
-            <h2 class="white--text font-weight-light">{{gameSessionGroup.key | date}}</h2>
-          </v-card-title>
-          <v-container>
-            <v-layout row wrap>
-              <v-flex class="pa-2" xs12 :key="gameSession.id" v-for="gameSession in gameSessionGroup.g">
-                <v-layout>
-                  <v-flex xs2>
-                    <v-img :src="gameSession.game.thumbnail" height="100px" contain></v-img>
-                  </v-flex>
-                  <v-flex xs9>
-                    <v-card-title primary-title>
-                      <div>
-                        <div class="headline">
-                          <router-link :to="{name : 'game-detail', params : { id : gameSession.game.id}}">{{ gameSession.game.name }}</router-link>
-                        </div>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs12 :key="gameSessionGroup.id" v-for="gameSessionGroup in gameSessionGroups">
+          <v-card>
+            <v-card-title class="primary lighten-1">
+              <h2 class="white--text font-weight-light">{{gameSessionGroup.key | date}}</h2>
+            </v-card-title>
+            <v-container>
+              <v-layout row wrap>
+                <v-flex class="pa-2" xs12 :key="gameSession.id" v-for="gameSession in gameSessionGroup.g">
+                  <v-layout>
+                    <v-flex xs2>
+                      <v-img :src="gameSession.game.thumbnail" height="100px" contain></v-img>
+                    </v-flex>
+                    <v-flex xs9>
+                      <v-card-title primary-title>
                         <div>
-                          <v-chip outline label :key="player.id" v-for="player in gameSession.players">
-                            <v-avatar tile class="white--text" :color="player.colour">{{player.name.charAt(0).toUpperCase()}}</v-avatar>
-                            <router-link :to="{name : 'player-profile', params : {id : player.id}}">{{player.name}}</router-link>
-                          </v-chip>
+                          <div class="headline">
+                            <router-link :to="{name : 'game-detail', params : { id : gameSession.game.id}}">{{ gameSession.game.name }}</router-link>
+                          </div>
+                          <div>
+                            <v-chip outline label :key="player.id" v-for="player in gameSession.players">
+                              <v-avatar tile class="white--text" :color="player.colour">{{player.name.charAt(0).toUpperCase()}}</v-avatar>
+                              <router-link :to="{name : 'player-profile', params : {id : player.id}}">{{player.name}}</router-link>
+                            </v-chip>
+                          </div>
                         </div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
-                  <v-flex xs1>
-                    <v-btn fab small :to="{name : 'game-session-update', params : { id : gameSession.id}}" color="secondary">
-                      <v-icon>edit</v-icon>
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-timeline-item>
-    </v-timeline>
+                      </v-card-title>
+                    </v-flex>
+                    <v-flex xs1>
+                      <v-btn left fab small :to="{name : 'game-session-update', params : { id : gameSession.id}}" color="secondary">
+                        <v-icon>edit</v-icon>
+                      </v-btn>
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
