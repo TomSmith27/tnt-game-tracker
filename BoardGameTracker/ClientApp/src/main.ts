b4c8f5b5d94b56e4f9516e7c6d9ae0bb126693e5
Tom Smith <tom.smith2711@gmail.com>
2019-06-13 23:11:04 +0100

    make tnt-tracker a PWA

--- BoardGameTracker/ClientApp/src/main.ts
+++ BoardGameTracker/ClientApp/src/main.ts
@@ -3,6 +3,7 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
 import StarRating from 'vue-star-rating';
 import moment from 'moment';
 import _ from 'lodash';
